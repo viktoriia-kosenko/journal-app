4dd298320f64d40a58edd641ca95a33d0f2d7c94
Vicky <kosenchiha@gmail.com>
2019-08-12 15:48:20 +0100

    display SidnedInLinks, when a user is logged in and SignedOtLinks when user is signed out. Prevent rendering app before firebase authentication is ready

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -5,21 +5,21 @@ import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
 const Navbar = props => {
+  const { auth } = props;
+  console.log(auth);
   return (
     <nav className="nav-wrapper blue-grey darken-1">
       <div className="container">
         <Link to="/" className="brand-logo left">
           Digital Journal
         </Link>
-        <LoggedInLinks />
-        <SignedOutLinks />
+        {auth.uid ? <LoggedInLinks /> : <SignedOutLinks />}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = state => {
-  console.log(state);
   return {
     auth: state.firebase.auth
   };
